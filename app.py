7bb3a7df024c107f9225d911e83e6c6f69aebe44
HanKun113 <KHAN009@e.ntu.edu.sg>
2024-12-09 08:25:57 +0000

    1

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask,request,render_template
 from flask import render_template,request
 import textblob
 import os
+import google.generativeai as genai
 
+genai.configure(api_key = "AIzaSyASMX-v6vczn9mF6k8jIBlQTQKiOaoj76A")
+model = genai.GenerativeModel("gemini-1.5-flash")
 # api_key=os.getenv("maskersuite")
 # palm.configure(api_key=api_key)
 
@@ -31,6 +34,16 @@ def SA_result():
     r = textblob.TextBlob(q).sentiment
     return render_template("SA_result.html",r=r)
 
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    return(render_template("genAI.html"))
+
+@app.route("/genAI_result", methods=["GET", "POST"])
+def genAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return render_template("genAI_result.html",r=r.candidates[0].content.parts[0].text)
+
 if __name__ == "__main__":
-    app.run(port=1111)
+    app.run()
 
