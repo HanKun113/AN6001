463faaaf595312ccdda3e5bf736e449918ec3833
HanKun113 <KHAN009@e.ntu.edu.sg>
2024-12-09 07:13:29 +0000

    1

--- app.py
+++ app.py
@@ -10,20 +10,22 @@ def index():
         a = request.form['options']
         print(a)
     return(render_template("index.html"))
+
 @app.route("/main",methods=["GET","POST"])
 def main():
     name = request.form.get("q")
-    return(render_template("index.html"))
+    return(render_template("main.html"))
   
 @app.route("/SA",methods=["GET","POST"])
 def SA():
     return(render_template("SA.html"))
   
-@app.route("/SA",methods=["GET","POST"])
-def SA():
-    q = request.form.get['q']
-    r = 
-    return(render_template("index.html"))
+@app.route("/SA_result", methods=["GET", "POST"])
+def SA_result():
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
+    return render_template("SA_result.html",r=r)
 
 if __name__ == "__main__":
     app.run(port=1111)
+
