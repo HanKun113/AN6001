6a2e2dab5b37fe41add2a456827b0aa1ae371eb5
HanKun113 <KHAN009@e.ntu.edu.sg>
2024-12-09 14:56:07 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        a = request.form['options']
+        print(a)
+    return(render_template("index.html"))
+@app.route("/main",methods=["GET","POST"])
+def main():
+    name = request.form.get("q")
+    return(render_template("index.html"))
+  
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+  
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    q = request.form.get['q']
+    r = 
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run(port=1111)
